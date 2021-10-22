960e76a339b006451556265192b025411a001ba3
Hitesh Gohil <92030259+hitesh0209@users.noreply.github.com>
2021-10-22 18:09:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+import { Button, Card, Image } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState("");
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+
+  return (
+    <div className="container">
+      <Card style={{ width: "600px" }}>
+        <Card.Header>
+          {!login && (
+            <FacebookLogin
+              appId="fb_app_id"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile"
+              callback={responseFacebook}
+              icon="fa-facebook"
+            />
+          )}
+          {login && <Image src={picture} roundedCircle />}
+        </Card.Header>
+        {login && (
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>{data.email}</Card.Text>
+            <Button
+              onClick={() => {
+                window.FB.logout();
+                setLogin(false);
+                console.log("logout");
+              }}
+            >
+              Logout
+            </Button>
+          </Card.Body>
+        )}
+      </Card>
+    </div>
+  );
+}
+
+export default App;
